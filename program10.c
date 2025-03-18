2a78746cb8fb24bc47dfb31acb0d5afd1232d9aa
gauritechcode <gaurinimbalkar2003@gmail.com>
2025-03-18 08:59:59 +0530

    character

---
+++ program10.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main()
+{
+char a[10];
+char b[20];
+char c[30];
+printf("Enter the value");
+scanf("%s",&a);
+printf("Enter the value");
+scanf("%s",&b);
+printf("Enter the value");
+scanf("%s",&c);
+printf("############\n");
+printf("firstName=%s\nmiddleName=%s\nlastName=%s",a,b,c);
+
+
+
+
+
+
+
+
+}
