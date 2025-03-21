9ab516c05787dc44676752013800218db5cd4f99
gauritechcode <gaurinimbalkar2003@gmail.com>
2025-03-21 09:04:19 +0530

    for loop

---
+++ for program 1.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+void main()
+{
+int c;
+printf("Enter the value");
+scanf("%d",&c);
+for(int i=1; i<=c; i++)
+{
+printf("%d\n",i);
+}
+
+for(int i=c; i>=0; i--)
+{
+printf("%d\n",i);
+}
+
+
+
+
+
+
+
+
+
+
+}
+
