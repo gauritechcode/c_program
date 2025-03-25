3cfb9d3a5a8186d9eef94cbddca05dc97771e223
gauritechcode <gaurinimbalkar2003@gmail.com>
2025-03-25 09:14:58 +0530

    for if continue

---
+++ 5.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+void main()
+{
+int a;
+printf("Enter the value");
+scanf("%d",&a);
+
+for(int i=1; i<=a;i++)
+{
+if(i==10)
+{
+continue;
+}
+
+
+printf("%d\n",i);
+}
+
+
+
+
+
+
+
+
+
+
+
+}
