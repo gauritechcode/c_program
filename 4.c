a1a66ee999a27234fefe69be50ec7daa0635cd1e
gauritechcode <gaurinimbalkar2003@gmail.com>
2025-03-25 09:14:15 +0530

    for if condition

---
+++ 4.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+void main()
+{
+int c;
+printf("Enter the value");
+scanf("%d",&c);
+
+for(int i=1; i<=c; i++)
+{
+if(5<i)
+{
+printf("%d\n",i);
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
