13b3ffaf76b07a1362c605d442b05b5d81adf14f
gauritechcode <gaurinimbalkar2003@gmail.com>
2025-04-02 08:21:50 +0530

    for paturn printing

---
+++ 2.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+void main()
+{
+int b;
+printf("enter the value");
+scanf("%d",&b);
+
+for(int i=0;i<=b; i++)
+{
+
+ for(int j=0; j<=b; j++){
+
+    printf("%d ",j);
+ }
+
+printf("\n");
+
+}
+
+
+
+
+
+
+
+
+
+}
