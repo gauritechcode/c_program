79189db99aa789456e2d97646bc2ddf0be16ee32
gauritechcode <gaurinimbalkar2003@gmail.com>
2025-04-02 08:22:44 +0530

    for decrement program

---
+++ for program decrement.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void main()
+{
+int a;
+printf("enter the value");
+scanf("%d",&a);
+
+for(int i=a; i>=0;i--)
+
+{
+   if(5<i)
+   {
+   printf("%d\n",i);
+   }
+
+}
+
+
+
+
+
+
+
+}
