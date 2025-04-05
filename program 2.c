3064ed9b59c3824a2b47587f1616f2b03dc2ae0d
gauritechcode <gaurinimbalkar2003@gmail.com>
2025-04-05 08:28:16 +0530

    if else ladder

--- program 2.c
+++ program 2.c
@@ -1,16 +1,37 @@
 #include<stdio.h>
 void main()
 {
+int mark;
+printf("enter the mark");
+scanf("%d",&mark);
 
-for(int i=0; i<=25; i++)
+if(mark>80)
 {
-printf("%c\n",i+65);  //65 =A 66= b
-
+ printf("congratulation !!!! your grade is A+");
 }
-
-
-
-
-
+else if(mark>70)
+{
+  printf("congratulation !!!! your grade is A");
+}
+else if(mark>60)
+{
+ printf("congratulation !!!! your grade is B+");
+}
+else if(mark>50)
+{
+  printf("congratulation !!!! your grade is B");
+}
+else if(mark>40)
+{
+  printf("congratulation !!!! your grade is C+");
+}
+else if(mark>35)
+{
+ printf("congratulation !!!! your grade is C");
+}
+else
+{
+printf("fail");
+}
 
 }
