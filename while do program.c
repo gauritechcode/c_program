35624e73f384a14761ea1a567da66869113f6ac8
gauritechcode <gaurinimbalkar2003@gmail.com>
2025-04-22 08:28:16 +0530

    do while program

---
+++ while do program.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+int a=5;
+int j=0;
+int i=0;
+while(i<=a)
+{
+printf("My Name is Gauri\n");
+i++;
+}
+
+do
+{
+printf("Hello India\n");
+j++;
+}while(j<=a);
+
+
+
+}
